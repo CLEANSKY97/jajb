0420ca485195afa2f051b013b514bb6629db426a
kazuhiko.arase <kazuhiko.arase@gmail.com>
2018-10-05 00:46:07 +0900

    refactor

--- src/test/java/com/d_project/jajb/JSONTest.java
+++ src/test/java/com/d_project/jajb/JSONTest.java
@@ -20,22 +20,22 @@ public class JSONTest {
     Object handle(Object obj);
   }
 
-  protected void test(String src) throws Exception{
-    test(src, src);
+  protected void assertJSON2JSON(String src) throws Exception{
+    assertJSON2JSON(src, src);
   }
 
-  protected void test(String src, String expected) throws Exception {
+  protected static void assertJSON2JSON(String src, String expected) throws Exception {
     String actual = JSON.stringify(JSON.parse(src) );
     Assert.assertEquals(expected, actual);
   }
 
-  protected void test(String src,
+  protected static void assertJSON2JSON(String src,
       Class<?> targetClass,
       ObjectHandler h) throws Exception{
-    test(src, src, targetClass, h);
+    assertJSON2JSON(src, src, targetClass, h);
   }
 
-  protected void test(String src, String expected,
+  protected static void assertJSON2JSON(String src, String expected,
       Class<?> targetClass,
       ObjectHandler h) throws Exception {
     String actual = JSON.stringify(h.handle(JSON.parse(src, targetClass) ) );
@@ -45,43 +45,43 @@ public class JSONTest {
   @Test
   public void testPlain() throws Exception {
 
-    test("null");
-    test(" null ", "null");
-    test("true");
-    test("false");
-    test("NaN");
-    test("+1E2", "1E+2");
-    test("-1E2", "-1E+2");
-    test("\"\"","\"\"");
-    test("\"a\"","\"a\"");
-    test(" \"a\"","\"a\"");
-    test("\"\\u3000\"","\"\u3000\"");
+    assertJSON2JSON("null");
+    assertJSON2JSON(" null ", "null");
+    assertJSON2JSON("true");
+    assertJSON2JSON("false");
+    assertJSON2JSON("NaN");
+    assertJSON2JSON("+1E2", "1E+2");
+    assertJSON2JSON("-1E2", "-1E+2");
+    assertJSON2JSON("\"\"","\"\"");
+    assertJSON2JSON("\"a\"","\"a\"");
+    assertJSON2JSON(" \"a\"","\"a\"");
+    assertJSON2JSON("\"\\u3000\"","\"\u3000\"");
 
   }
 
   @Test
   public void testObjects() throws Exception {
 
-    test("[1,20,-3]");
-    test(" [01, 020 ,-03] ","[1,20,-3]");
-    test("\"\\u3000\"","\"\u3000\"");
-    test("[0.5,-0.5,0.3]");
-    test("[\"1\",\"2\",\"3\"]");
-    test("[\"\\b\\f\\n\\t\\r\"]");
+    assertJSON2JSON("[1,20,-3]");
+    assertJSON2JSON(" [01, 020 ,-03] ","[1,20,-3]");
+    assertJSON2JSON("\"\\u3000\"","\"\u3000\"");
+    assertJSON2JSON("[0.5,-0.5,0.3]");
+    assertJSON2JSON("[\"1\",\"2\",\"3\"]");
+    assertJSON2JSON("[\"\\b\\f\\n\\t\\r\"]");
 
   }
 
   @Test
   public void testComplex() throws Exception {
-    test("[\"\\n\\t\\r\",[\"1\",\"2\",\"3\"]]");
-    test("[{},{},{}]");
-    test("[1,2,[3],4,[5,6],7]");
-    test("{\"a\":[],\"c\":[],\"b\":[]}");
+    assertJSON2JSON("[\"\\n\\t\\r\",[\"1\",\"2\",\"3\"]]");
+    assertJSON2JSON("[{},{},{}]");
+    assertJSON2JSON("[1,2,[3],4,[5,6],7]");
+    assertJSON2JSON("{\"a\":[],\"c\":[],\"b\":[]}");
   }
 
   @Test
   public void testPOJO1() throws Exception {
-    test("{\"group\":null,\"items\":null,\"num\":1,\"str\":\"1\"}",
+    assertJSON2JSON("{\"group\":null,\"items\":null,\"num\":1,\"str\":\"1\"}",
         TestVO.class, new ObjectHandler() {
       @Override
       public Object handle(Object obj) {
@@ -98,7 +98,7 @@ public class JSONTest {
 
   @Test
   public void testPOJO2() throws Exception {
-    test("{\"group\":{}," +
+    assertJSON2JSON("{\"group\":{}," +
         "\"items\":[],\"num\":1,\"str\":\"1\"}",
         "{\"group\":{\"s1\":null,\"s2\":null}," +
         "\"items\":[],\"num\":1,\"str\":\"1\"}",
@@ -118,7 +118,7 @@ public class JSONTest {
 
   @Test
   public void testPOJO3() throws Exception {
-    test("{\"group\":{\"s1\":\"a\",\"s2\":\"b\"}," +
+    assertJSON2JSON("{\"group\":{\"s1\":\"a\",\"s2\":\"b\"}," +
         "\"items\":[{\"f1\":\"@\",\"f2\":2},{\"f1\":\"%\"}],\"num\":1,\"str\":\"1\"}",
         "{\"group\":{\"s1\":\"a\",\"s2\":\"b\"}," +
         "\"items\":[{\"f1\":\"@\"},{\"f1\":\"%\"}],\"num\":1,\"str\":\"1\"}",
@@ -143,7 +143,7 @@ public class JSONTest {
 
   @Test
   public void testPOJO4() throws Exception {
-    test("{\"arr\":[1,2,3],\"flg\":true,\"group\":null," +
+    assertJSON2JSON("{\"arr\":[1,2,3],\"flg\":true,\"group\":null," +
         "\"items\":null,\"num\":1,\"str\":\"1\",\"str2\":\"2\"}",
         "{\"arr\":[1,2,3],\"flg\":true,\"group\":null," +
             "\"items\":null,\"num\":1}",
