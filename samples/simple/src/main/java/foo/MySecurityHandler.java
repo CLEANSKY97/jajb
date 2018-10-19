b51b482c14c6663fca2511e41f6bd1a721fb7846
kazuhiko.arase <kazuhiko.arase@gmail.com>
2018-10-19 21:09:53 +0900

    commit

---
+++ samples/simple/src/main/java/foo/MySecurityHandler.java
@@ -0,0 +1,24 @@
+package foo;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.d_project.jajb.rpc.SecurityHandler;
+
+public class MySecurityHandler implements SecurityHandler {
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+  }
+
+  @Override
+  public boolean isAuthorized(HttpServletRequest request,
+      Map<String, Object> opts, Method targetMethod) throws ServletException {
+    // allow all.
+    return true;
+  }
+}
